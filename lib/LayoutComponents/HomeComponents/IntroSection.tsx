c0e1e146bfbdfdeafa7f520d6ebb46926953f293
unknown <moatazmamdouh.js@gmail.com>
2025-12-14 15:12:33 +0200

    finished styling text section of intro section

--- lib/LayoutComponents/HomeComponents/IntroSection.tsx
+++ lib/LayoutComponents/HomeComponents/IntroSection.tsx
@@ -4,7 +4,7 @@ export default function IntroSection() {
   return (
     <>
       <section className="min-h-screen bg-black px-10 flex justify-center items-center">
-        <div className="flex flex-col gap-5 justify-start">
+        <div className="flex flex-col gap-8 justify-start w-1/2">
           <h1 className="text-6xl text-gray-200 font-extrabold">
             WE MAKE{" "}
             <span className="text-[#BD50FD]">
@@ -19,18 +19,18 @@ export default function IntroSection() {
             </span>
             .
           </h1>
-          <p className="text-[#9898A1]">
+          <p className="text-[#9898A1] text-xl">
             We&apos;re the place where brands come when they&apos;re ready to
             become memorable. We blur the line between commerce and creation,
             crafting identities that don&apos;t shout, but echo.
           </p>
-          <p className="text-[#9898A1]">
+          <p className="text-[#9898A1] text-xl">
             Unlike agencies that chase fleeting trends, we create work that
             lives. Not content that vanishes in the scroll, but stories that
             stay with you.
           </p>
         </div>
-        <div className="w-10 bg-white">tatda</div>
+        <div className="w-1/2 bg-white">tatda</div>
       </section>
     </>
   );
