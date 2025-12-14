657b449ab9a889729e0e82ce43a335bf38f2dfa1
unknown <moatazmamdouh.js@gmail.com>
2025-12-14 14:44:05 +0200

    finished hero section

--- lib/LayoutComponents/HomeComponents/HeroSection.tsx
+++ lib/LayoutComponents/HomeComponents/HeroSection.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <>
-      <main className="min-h-screen bg-black flex flex-col gap-6">
+      <main className="min-h-screen bg-black flex flex-col gap-4 md:gap-6">
         <div className="flex items-center justify-center">
           <Badge variant="secondary" className="bg-[#0D0D0D] text-[#723392] ">
             Creative Advertising Agency
           </Badge>
         </div>
         <div className="text-center">
-          <h1 className="text-white text-9xl font-bold tracking-tight">
+          <h1 className="text-white text-5xl md:text-9xl font-extrabold tracking-tight">
             VISION
           </h1>
-          <h1 className="text-9xl font-extrabold bg-linear-to-r from-[#7793FF] to-[#A6A6A6] bg-clip-text text-transparent tracking-tight">
+          <h1 className="text-5xl md:text-9xl font-extrabold bg-linear-to-r from-[#7793FF] to-[#A6A6A6] bg-clip-text text-transparent tracking-tight">
             UNBOUND
           </h1>
         </div>
-        <div className=" text-center ">
-          <p className="text-[#A6A6A6] text-xl">
+        <div className="text-center">
+          <p className="text-[#A6A6A6] text-lg md:text-xl">
             We are the architects of the unseen. DBH blends diverse artistic
             prowess
             <br /> with cutting-edge technology to deliver immersive experiences
