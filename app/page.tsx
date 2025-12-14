4a158ebb1169147e0bb12a1de80a9512829fe061
unknown <moatazmamdouh.js@gmail.com>
2025-12-14 15:06:36 +0200

    finished styling headers of intro section and layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import HeroSection from "@/lib/LayoutComponents/HomeComponents/HeroSection";
+import IntroSection from "@/lib/LayoutComponents/HomeComponents/IntroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-
+      <IntroSection />
       <h1>HIII DBH LETS GOOOOOOOO</h1>
     </>
   );
--- lib/LayoutComponents/HomeComponents/IntroSection.tsx
+++ lib/LayoutComponents/HomeComponents/IntroSection.tsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function IntroSection() {
-  return <div>IntroSection</div>;
+  return (
+    <>
+      <section className="min-h-screen bg-black px-10 flex justify-center items-center">
+        <div className="flex flex-col gap-5 justify-start">
+          <h1 className="text-6xl text-gray-200 font-extrabold">
+            WE MAKE{" "}
+            <span className="text-[#BD50FD]">
+              <i>SENSE</i>
+            </span>
+            .
+          </h1>
+          <h1 className="text-6xl text-gray-200 font-extrabold">
+            NOT JUST{" "}
+            <span className="bg-linear-to-r from-[#00AAFF] to-[#AB49FF] bg-clip-text text-transparent">
+              LOOKS
+            </span>
+            .
+          </h1>
+          <p className="text-[#9898A1]">
+            We&apos;re the place where brands come when they&apos;re ready to
+            become memorable. We blur the line between commerce and creation,
+            crafting identities that don&apos;t shout, but echo.
+          </p>
+          <p className="text-[#9898A1]">
+            Unlike agencies that chase fleeting trends, we create work that
+            lives. Not content that vanishes in the scroll, but stories that
+            stay with you.
+          </p>
+        </div>
+        <div className="w-10 bg-white">tatda</div>
+      </section>
+    </>
+  );
 }
